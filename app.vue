3eb278bed337f9c30a9f96cc9e5146c6a49188f8
Anthony Dionise <anthony.dionise@gmail.com>
2023-10-26 14:30:59 -0400

    initiate search bar

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-[#334254] px-4 h-screen pt-5">
+    <div class="flex justify-center ...">
+      <input class="w-screen max-w-7xl rounded overflow-hidden shadow-lg h-10">
+    </div>
   </div>
 </template>
